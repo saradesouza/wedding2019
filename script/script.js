d3f5e9bb945b1deb47269096c9d6ec817c21f0be
Sara de Souza <saradesouza@The-Cheese-Machine.local>
2019-05-02 02:52:00 +0800

    add countdown timer, clean up RSVP section

--- script/script.js
+++ script/script.js
@@ -1,5 +1,5 @@
 $(document).ready(() => {
-  console.log('hi');
+  console.log('hello');
   // SMOOTH SCROLL===
   // Select all links with hashes
   $('a[href*="#"]')
@@ -10,7 +10,7 @@ $(document).ready(() => {
       // On-page links
       if (
         location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
-				&&				location.hostname == this.hostname
+        &&        location.hostname == this.hostname
       ) {
         // Figure out element to scroll to
         let target = $(this.hash);
@@ -30,9 +30,39 @@ $(document).ready(() => {
               return false;
             }
             $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
-			$target.focus(); // Set focus again				
-		});
+      $target.focus(); // Set focus again       
+    });
         }
       }
     });
 });
+
+
+// Set the date we're counting down to
+var countDownDate = new Date("Dec 1, 2019 10:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get todays date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("countdown-clock").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown-clock").innerHTML = "EXPIRED";
+  }
+}, 1000);
