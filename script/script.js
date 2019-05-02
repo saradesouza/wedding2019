c60099677747018611441b81dee66350d9d8b812
Sara de Souza <saradesouza@The-Cheese-Machine.local>
2019-05-02 23:20:39 +0800

    Add I'm here at your reception section

--- script/script.js
+++ script/script.js
@@ -97,7 +97,7 @@ var x = setInterval(function() {
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Display the result in the element with id="demo"
-  document.getElementById("countdown-clock").innerHTML = "(" + days + " days " + hours + " hours "
+  document.getElementById("countdown-clock").innerHTML = "(" + days + " days, " + hours + " hours and "
   + minutes + " minutes to go!)";
 
   // If the count down is finished, write some text 
